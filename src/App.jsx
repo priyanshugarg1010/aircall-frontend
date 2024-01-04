6bff89aa732eeaa24a67790d6cda133f7047c92a
Priyanshu Garg <priyanshugarg1810@gmail.com>
2024-01-04 09:59:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import "./app.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <div className="">
+        <Footer />{" "}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+import "./header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header">
+        <div className="app_icon">
+          <img
+            src="../public/phone.png"
+            alt="phone icon"
+            className="app_icon_img"
+          />
+          <h1 className="header_heading">Activity</h1>
+        </div>
+
+        <div className="inbox">Inbox</div>
+        <div className="all_calls">All calls</div>
+        <div className="filter_img">
+          <img src="../public/filter.svg" alt="" />
+        </div>
+      </div>
+      <hr className="hr_line" />
+    </header>
+  );
+};
+
+export default Header;
