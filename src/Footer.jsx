dce727e9863d0db17b61a096bccfbf95dd7e4312
Priyanshu Garg <priyanshugarg1810@gmail.com>
2024-01-04 10:07:45 +0530

    modify the image path

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,4 +1,9 @@
 import "./footer.css";
+import functionimg from "/function.png";
+import setting from "/settings.png";
+import dial from "/dial.png";
+import phone_call from "/phone-call.png";
+import user from "/user.png";
 
 const Footer = () => {
   return (
@@ -6,19 +11,19 @@ const Footer = () => {
       <hr className="hr_line" />
       <div className="footer">
         <div className="">
-          <img src="../public/phone-call.png" alt="" className="img" />
+          <img src={phone_call} alt="" className="img" />
         </div>
         <div className="">
-          <img src="../public/user.png" alt="" className="img" />
+          <img src={user} alt="" className="img" />
         </div>
         <div className="dial">
-          <img src="../public/dial.png" alt="" className="dial_img" />
+          <img src={dial} alt="" className="dial_img" />
         </div>
         <div className="">
-          <img src="../public/settings.png" alt="" className="img" />
+          <img src={setting} alt="" className="img" />
         </div>
         <div className="">
-          <img src="../public/function.png" alt="" className="img" />
+          <img src={functionimg} alt="" className="img" />
         </div>
       </div>
     </footer>
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,22 +1,20 @@
 import "./header.css";
+import phone from "/phone.png";
+import filter from "/filter.svg";
 
 const Header = () => {
   return (
     <header>
       <div className="header">
         <div className="app_icon">
-          <img
-            src="../public/phone.png"
-            alt="phone icon"
-            className="app_icon_img"
-          />
+          <img src={phone} alt="phone icon" className="app_icon_img" />
           <h1 className="header_heading">Activity</h1>
         </div>
 
         <div className="inbox">Inbox</div>
         <div className="all_calls">All calls</div>
         <div className="filter_img">
-          <img src="../public/filter.svg" alt="" />
+          <img src={filter} alt="" />
         </div>
       </div>
       <hr className="hr_line" />
